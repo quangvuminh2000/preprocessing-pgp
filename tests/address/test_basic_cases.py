ecf7d74571ceb3b787b146fcc6165569ee9cf162
quangvm2000 <quang.vuminh@hcmut.edu.vn>
2022-12-19 14:07:23 +0700

    Remove tests folder from package build & Add test cases for non-accented address

--- tests/address/test_basic_cases.py
+++ tests/address/test_basic_cases.py
@@ -20,6 +20,7 @@ class TestLevelExtraction:
     extractor = level_extractor.LevelExtractor()
     cleaner = preprocess.VietnameseAddressCleaner()
 
+    # * Testing for level 1
     def test_enriched_lv1_extraction(self):
         """
         Test it can extract level 1 correctly from a string of address
@@ -31,6 +32,25 @@ class TestLevelExtraction:
         address_data = '''658/11 Trương Công Định,
         Phường Nguyễn An Ninh,
         Thành Phố Vũng Tàu, Tỉnh Bà Rịa - Vũng Tàu'''
+
+        cleaned_addr = self.cleaner.clean_address(address_data)
+
+        _, _, best_lvs = self.extractor.extract_all_levels(
+            cleaned_addr)
+
+        assert best_lvs[1] == 'Tỉnh Bà Rịa - Vũng Tàu'
+
+    def test_non_accented_lv1_extraction(self):
+        """
+        Test it can extract level 1 correctly from a string of address
+
+        * Level 1 is non-accented and clean
+        * The address not containing any abbreviation
+        """
+
+        address_data = '''658/11 Truong Cong Dinh,
+        Phuong Nguyen An Ninh,
+        Thanh Pho Vung Tau, Tinh Ba Ria - Vung Tau'''
 
         cleaned_addr = self.cleaner.clean_address(address_data)
 
@@ -58,6 +78,25 @@ class TestLevelExtraction:
 
         assert best_lvs[2] == 'Thành phố Vũng Tàu'
 
+    def test_non_accented_lv2_extraction(self):
+        """
+        Test it can extract level 2 correctly from a string of address
+
+        * Level 2 is non-accented and clean
+        * The address not containing any abbreviation
+        """
+
+        address_data = '''658/11 Truong Cong Dinh,
+        Phuong Nguyen An Ninh,
+        Thanh Pho Vung Tau, Tinh Ba Ria - Vung Tau'''
+
+        cleaned_addr = self.cleaner.clean_address(address_data)
+
+        _, _, best_lvs = self.extractor.extract_all_levels(
+            cleaned_addr)
+
+        assert best_lvs[2] == 'Thành phố Vũng Tàu'
+
     def test_enriched_lv3_extraction(self):
         """
         Test it can extract level 3 correctly from a string of address
@@ -77,6 +116,25 @@ class TestLevelExtraction:
 
         assert best_lvs[3] == 'Phường Nguyễn An Ninh'
 
+    def test_non_accented_lv3_extraction(self):
+        """
+        Test it can extract level 3 correctly from a string of address
+
+        * Level 3 is non-accented and clean
+        * The address not containing any abbreviation
+        """
+
+        address_data = '''658/11 Truong Cong Dinh,
+        Phuong Nguyen An Ninh,
+        Thanh Pho Vung Tau, Tinh Ba Ria - Vung Tau'''
+
+        cleaned_addr = self.cleaner.clean_address(address_data)
+
+        _, _, best_lvs = self.extractor.extract_all_levels(
+            cleaned_addr)
+
+        assert best_lvs[3] == 'Phường Nguyễn An Ninh'
+
     def test_enriched_remained_addr_extraction(self):
         """
         Test it can extract remaining address nearly correct from a string of address
@@ -88,6 +146,25 @@ class TestLevelExtraction:
         address_data = '''658/11 Trương Công Định,
         Phường Nguyễn An Ninh,
         Thành Phố Vũng Tàu, Tỉnh Bà Rịa - Vũng Tàu'''
+
+        cleaned_addr = self.cleaner.clean_address(address_data)
+
+        _, remained_address, _ = self.extractor.extract_all_levels(
+            cleaned_addr)
+
+        assert remained_address.find('658/11 truong cong dinh') != -1
+
+    def test_non_accented_remained_addr_extraction(self):
+        """
+        Test it can extract remaining address nearly correct from a string of address
+
+        * Remaining address is the remained string after extraction
+        * The address not containing any abbreviation
+        """
+
+        address_data = '''658/11 Truong Cong Dinh,
+        Phuong Nguyen An Ninh,
+        Thanh Pho Vung Tau, Tinh Ba Ria - Vung Tau'''
 
         cleaned_addr = self.cleaner.clean_address(address_data)
 
