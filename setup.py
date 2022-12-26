56bbf4e9474679be27b7f53428a86055c517d0cd
quangvm2000 <quang.vuminh@hcmut.edu.vn>
2022-12-26 11:35:05 +0700

    Add MANIFEST.in to fix bug missing vector files

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 # ? setup() does all the work
 setup(
-    include_dirs=['preprocessing_pgp/data'],
-    include_package_data=True,
-    package_data={
-        "preprocessing_pgp.data": ["*"]
-    }
+    # include_dirs=['preprocessing_pgp/data'],
+    # include_package_data=True,
+    # package_data={
+    #     "preprocessing_pgp.data": ["*"]
+    # }
 )
