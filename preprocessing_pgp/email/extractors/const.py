2ef3a24d023ede76680822e2f59ea4cc580faccd
quangvm2000 <quang.vuminh@hcmut.edu.vn>
2023-02-03 15:49:42 +0700

    Add email2yob class for further logic

--- preprocessing_pgp/email/extractors/const.py
+++ preprocessing_pgp/email/extractors/const.py
@@ -13,3 +13,22 @@ fullname_path = os.path.join(
 )
 
 FULLNAME_DICT = pd.read_parquet(fullname_path)
+
+# ? YOB
+YEAR_REGEX_DICT = {
+    'full_year': '(19[5-9][0-9]|200[0-9])',
+    'half_year': '([7-9][0-9])',
+    'full_month': '(0[1-9]|1[012])',
+    'half_month': '([1-9])',
+    'full_day': '(0[1-9]|[12][0-9]|3[01])',
+    'half_day': '([1-9])'
+}
+
+YEAR_FORMAT_DICT = {
+    'full_year': 'yyyy',
+    'half_year': 'yy',
+    'full_month': 'mm',
+    'half_month': 'm',
+    'full_day': 'dd',
+    'half_day': 'd'
+}
---
+++ preprocessing_pgp/email/extractors/email_yob_extractor.py
@@ -0,0 +1,7 @@
+"""
+Module to extract YOB from email using rule-based
+"""
+
+class EmailYOBExtractor:
+    def __init__(self):
+        pass
